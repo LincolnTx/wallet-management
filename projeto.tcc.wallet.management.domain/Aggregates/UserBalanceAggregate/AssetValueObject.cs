252875b964aa49dce2c1758901d5259a3e881a17
Lincoln Teixeira <lincoln@vexter.com.br>
2020-05-31 20:54:36 -0300

    adding new model and new Balance Aggregator

--- projeto.tcc.wallet.management.domain/Aggregates/UserBalanceAggregate/AssetValueObject.cs
+++ projeto.tcc.wallet.management.domain/Aggregates/UserBalanceAggregate/AssetValueObject.cs
@@ -7,18 +7,13 @@ namespace projeto.tcc.wallet.management.domain.Aggregates.UserBalanceAggregate
 	{
 		private string _name;
 		private string _symbol;
-		private string _imageUrl;
-		private string _segment;
 		private DateTime _startDate;
 		private decimal _startPrice;
 
-		public AssetValueObject(string name, string symbol, string imageUrl, string segment, DateTime startDate,
-			decimal startPrice)
+		public AssetValueObject(string name, string symbol, DateTime startDate, decimal startPrice)
 		{
 			_name = name;
 			_symbol = symbol;
-			_imageUrl = imageUrl;
-			_segment = segment;
 			_startDate = startDate;
 			_startPrice = startPrice;
 		}
