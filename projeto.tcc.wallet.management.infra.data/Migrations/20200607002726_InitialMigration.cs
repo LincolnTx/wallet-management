fd813dace590b8e3231a01946b06030a2a1239cf
Lincoln Teixeira <lincoln@vexter.com.br>
2020-06-06 22:17:58 -0300

    Initial migration

---
+++ projeto.tcc.wallet.management.infra.data/Migrations/20200607002726_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace projeto.tcc.wallet.management.infra.data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Balance",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TotalProperty = table.Column<decimal>(nullable: false),
+                    TrasientBalance = table.Column<decimal>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Balance", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Balance");
+        }
+    }
+}
